839e8feff39e6d9a79567819a41d1fba7930a35a
José Padilla <josecristhian@gmail.com>
2024-10-29 09:23:26 -0400

    Cleaning Base

--- src/app/apps/apps-settings.module.ts
+++ src/app/apps/apps-settings.module.ts
@@ -2,17 +2,12 @@
 import { Routes } from '@angular/router';
 
 export const APPS_SETTINGS = [
-    { id: 'app00000', app: 'default', default: false, loginSource: 'usuario/login/', tenant: 'bomboncitos' },
-    { id: 'app00100', app: 'app00100', default: false, loginSource: 'usuario/login/', tenant: 'bomboncitos' },
-    { id: 'app00101', app: 'fundme', default: true, allowPublicRoutes: true, loginSource: 'usuario/login/', tenant: 'bomboncitos' }
+    { id: 'app00000', app: 'default', default: true, allowPublicRoutes: true, loginSource: 'usuario/login/', tenant: 'bomboncitos' }
 ];
 
 export const APPS_ROUTES_PRIVATE: Routes = [
     { path: 'app00000', loadChildren: () => import('../_waicato-engine/_default-app/app-routing.module').then(m => m.RoutingModule) },
-    { path: 'app00100', loadChildren: () => import('./apps/app00100/app-routing.module').then(m => m.RoutingModule) },
-    { path: 'fundme', loadChildren: () => import('./apps/app00101/app-routing.module').then(m => m.RoutingModule) },
 ];
 
 export const APPS_ROUTES_PUBLIC: Routes = [
-    { path: '', loadChildren: () => import('./apps/app00101/app-routing-public.module').then(m => m.RoutingModule) },
 ];
--- src/app/apps/templates-settings.module.ts
+++ src/app/apps/templates-settings.module.ts
@@ -3,5 +3,5 @@ import { Template00002Component } from './templates/template00002/main/main.comp
 import { Template00002LoginComponent } from './templates/template00002/login/login.component';
 
 export const TEMPLATE_LIST = [
-    { id: 'template-00002', default: true, base: Template00002Component, login: Template00002LoginComponent }
+    { id: 'template-00002', default: false, base: Template00002Component, login: Template00002LoginComponent }
 ];
