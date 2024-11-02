23d6299fa1040bc3a4753e0c48c1047a23c47f45
José Padilla <josecristhian@gmail.com>
2024-11-02 10:30:06 -0400

    Small changes

--- src/app/apps/templates-settings.module.ts
+++ src/app/apps/templates-settings.module.ts
@@ -1,7 +1,10 @@
-	
+/*	
 import { Template00002Component } from './templates/template00002/main/main.component';
 import { Template00002LoginComponent } from './templates/template00002/login/login.component';
 
 export const TEMPLATE_LIST = [
     { id: 'template-00002', default: true, base: Template00002Component, login: Template00002LoginComponent }
 ];
+*/
+
+export const TEMPLATE_LIST = [];
